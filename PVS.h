3f4625565b824451add57ac1f787332fc991cbc0
Tony Tran <trant8381@wrdsb.ca>
2024-01-26 17:01:37 -0500

    more logging

--- PVS.h
+++ PVS.h
@@ -20,10 +20,16 @@ inline int searchBlock(bool& bSearchPv, int& beta, int& alpha, int& depth, std::
 
 inline int evaluate(std::stack<Position>& movelist, NNUE& model) {
 	std::array<torch::Tensor, 2> halfkp = movelist.top().halfkp();
-	std::cout << "halfkp" << std::endl;
-	std::cout << halfkp[0].size(0) << std::endl;
-	std::cout << halfkp[1].size(0) << std::endl;
-	torch::Tensor output = model->forward(halfkp[0].to_dense().unsqueeze_(0), halfkp[1].to_dense().unsqueeze_(0));
+
+	torch::Tensor denseHalf1 = halfkp[0].to_dense();
+	std::cout << "dense1" << std::endl;
+	torch::Tensor denseHalf2 = halfkp[1].to_dense();
+	std::cout << "dense2" << std::endl;
+	torch::Tensor half1 = denseHalf1.unsqueeze_(0);
+	std::cout << "half1" << std::endl;
+	torch::Tensor half2 = denseHalf2.unsqueeze_(0);
+	std::cout << "half2" << std::endl;
+	torch::Tensor output = model->forward(half1, half2);
 	std::cout << output << std::endl;
 	int eval = output[0][0].item().to<int>();
 	return eval;
