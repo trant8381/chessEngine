8b33d7d9840ec2cb9c8cf81496b4e52d0891ab6b
Tony Tran <trant8381@wrdsb.ca>
2024-01-26 16:32:55 -0500

    log everywhere

--- PVS.h
+++ PVS.h
@@ -19,10 +19,13 @@ inline int searchBlock(bool& bSearchPv, int& beta, int& alpha, int& depth, std::
 }
 
 inline int evaluate(std::stack<Position>& movelist, NNUE& model) {
+	std::cout << "here" << std::endl;
 	std::array<torch::Tensor, 2> halfkp = movelist.top().halfkp();
+	std::cout << "halfkp" << std::endl;
 	torch::Tensor output = model->forward(halfkp[0].to_dense().unsqueeze_(0), halfkp[1].to_dense().unsqueeze_(0));
 	std::cout << output << std::endl;
 	int eval = output[0][0].item().to<int>();
+	std::cout << eval << std::endl;
 	return eval;
 }
 
